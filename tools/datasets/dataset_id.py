196da6e690c796531a017c5c0156b34012cd6d5d
Yan Feng <yanfeng615@outlook.com>
2024-03-17 15:42:00 +0900

    add tools and models

---
+++ tools/datasets/dataset_id.py
@@ -0,0 +1,15 @@
+ID2DATASET={
+    0: 'PIE',
+    1: 'JAAD',
+    2: 'TITAN',
+    3: 'nuscenes',
+    4: 'bdd100k',
+}
+
+DATASET2ID={
+    'PIE': 0,
+    'JAAD': 1,
+    'TITAN': 2,
+    'nuscenes': 3,
+    'bdd100k': 4,
+}
